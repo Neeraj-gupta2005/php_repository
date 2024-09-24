d87b57242a5f54c09b55ad49b55e0987a40c699d
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-24 14:46:32 +0530

    added favicon

--- register.php
+++ register.php
@@ -4,6 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/svg+xml" href="images/logo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="css/signin.css">
